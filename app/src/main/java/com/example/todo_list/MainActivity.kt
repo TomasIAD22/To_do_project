0083549b84f1b3ff13023cc2bb81a3e0748f7751
TomasIAD22 <Tomas.Hikmet@stud.sti.se>
2023-04-24 09:55:14 +0200

    Added functionality to the Login button

--- app/src/main/java/com/example/todo_list/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.todo_list
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnNavigate: Button = findViewById(R.id.btn_loggin)
+
+        btnNavigate.setOnClickListener{
+            val intentNavigate = Intent(this, LoginActivity().javaClass).apply {
+            }
+
+            startActivity(intentNavigate)}
+
     }
-}
+    }
+
