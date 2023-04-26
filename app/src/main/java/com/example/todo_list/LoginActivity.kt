6c1b9fdf4dac5a2c12ccfe2fcc8b6b6792b0abce
TomasIAD22 <Tomas.Hikmet@stud.sti.se>
2023-04-26 11:14:27 +0200

    Fixed button function for LoginActivity.kt and RegisterActivity.kt

--- app/src/main/java/com/example/todo_list/LoginActivity.kt
+++ app/src/main/java/com/example/todo_list/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.todo_list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val btnNavigate: Button = findViewById(R.id.btn_submit)
+        btnNavigate.setOnClickListener {
+            val intentNavigate = Intent(this, Todo_List().javaClass).apply {}
+            startActivity(intentNavigate)
+
+        }
 
 
     }
--- app/src/main/java/com/example/todo_list/RegisterActivity.kt
+++ app/src/main/java/com/example/todo_list/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.todo_list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val btnNavigate: Button = findViewById(R.id.btn_register1)
+        btnNavigate.setOnClickListener {
+            val intentNavigate = Intent(this, LoginActivity().javaClass).apply {}
+            startActivity(intentNavigate)
+
+        }
     }
 }
